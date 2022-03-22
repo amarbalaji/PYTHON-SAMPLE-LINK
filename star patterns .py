1ab45af2b0c33227767ceb8304c959aec09968f7
amar balaji <amarbalajianu@gmail.com>
2022-03-22 11:53:45 +0530

    python_all_star_patterns

--- star patterns .py
+++ star patterns .py
@@ -1,26 +1,7 @@
-"""a = ["DATA","python","python","science","DATA","Machine Learning"]
-append_values = []
-for items in range(0, len(a)):
-    index_a = 0
-    a_value_pop = a.pop(index_a)
-    if(a_value_pop) in a :
-        append_values.append(a_value_pop)
-    else:
-        pass
-    index_a += 1
-
-print(append_values)"""
-"""print(chr(65))
-print(chr(90))"""
-"""n= 65
-for i in range(0,6):
-    for j in range(0,i+1):
-        print(chr(n))
-"""
 """
 STAR PATTERN IN PYTHON
 """
-"""n = int(input("Enter a number: "))
+n = int(input("Enter a number: "))
 for i in range(n):
     for j in range(n):
         print("*", end="  ")
@@ -28,39 +9,39 @@ for i in range(n):
 
 
 """"""
-RIGHT ANGLE TRIANGE PATTERN"""
+RIGHT ANGLE TRIANGE PATTERN
+"""
 
-"""n = int(input("Enter a number: "))
+n = int(input("Enter a number: "))
 for i in range(n):
     for j in range(i+1):
         print("*", end=" ")
-    print("")"""
+    print("")
 
-"""
 DECREASING TRIANGLE PATTERN
 """
-"""n = 5
+n = 5
 for i in range(n):
     for j in range(i,n):
         print("*", end=" ")
-    print("")"""
-"""n = 5
+    print("")
+n = 5
 for i in range(n):
     for j in range(i,n):
         print(" ",end=" ")
     for j in range(i+1):
         print("*",end=" ")
-    print("")"""
+    print("")
 
-"""n = 5
+n = 5
 for i in range(n):
     for i in range(i+1):
         print(" ", end=" ")
     for i in range(i,n):
         print("*", end=" ")
-    print("")"""
+    print("")
 
-"""n = 5
+n = 5
 for i in range(n):
     for j in range(i,n):
         print(" ", end=" ")
@@ -69,9 +50,9 @@ for i in range(n):
     for j in range(i):
         print("*", end=" ")
     print("")
-"""
 
-"""n = 5
+
+n = 5
 for i in range(n):
     for j in range(i+1):
         print(" ", end=" ")
@@ -79,7 +60,7 @@ for i in range(n):
         print("*", end=" ")
     for j in range(i,n-1):
         print("*", end=" ")
-    print("")"""
+    print("")
 
 n = 5
 for i in range(n-1):
